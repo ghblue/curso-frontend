26c7d6a2f2d5541ccbee6426184fc80226088052
ghblue <ghresilva@inf.ufpel.edu.br>
2021-03-10 17:33:57 -0300

    parte 1 desafio javascript.pt4

---
+++ aula javascript.pt4/funcoes_aula4.js
@@ -0,0 +1,6 @@
+const students = [
+    {id: 1, name: 'John Doe', age: 23, scholarship: false},
+    {id: 2, name: 'Margoe Rose', age: 19, scholarship: true},
+    {id: 3, name: 'Kayle', age: 22, scholarship: true}
+]
+
